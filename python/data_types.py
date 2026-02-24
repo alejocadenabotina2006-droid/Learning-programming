51da8e8114a255666532b3aff677fa6b0a24d8f4
mauricio cadena <waestrada.8643@unicesmag.edu.co>
2026-02-24 13:01:51 -0500

    tarea puthon

---
+++ python/data_types.py
@@ -0,0 +1,13 @@
+#this is my first python script
+'''
+    developer: Mauricio 
+    description: this script let us understand how to data types.
+'''
+print("hellooeveryon. welcome to data types class")
+
+age= 17
+print(type(age))
+EARTH_GRAVITY = 9.8
+pi = 3.1416
+print(type(EARTH_GRAVITY))
+print(type(pi))
---
+++ python/variables.py
@@ -0,0 +1,4 @@
+first_name= "Mauricio"
+print(type(first_name))
+print(first_name)
+print("your name is:",first_name)
